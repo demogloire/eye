4ed013e159bc6027a0aa2f9f65aea0cfed095caa
MUSA CHIRIMWAMI Gloire <chirimwami2014@gmail.com>
2021-07-06 17:14:20 +0200

    db

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,6 @@
 #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:josue@localhost:5432/standar_db'
-#SQLALCHEMY_DATABASE_URI = "mysql+pymysql://200181:Esperance1993@@mysql-chirimwami.alwaysdata.net/chirimwami_eye"
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='', server='localhost', database='eye')
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://200181:Esperance1993@@mysql-chirimwami.alwaysdata.net/chirimwami_eye"
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='', server='localhost', database='eye')
 
                             
 
